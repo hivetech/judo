aca83b3d8dafdf612b9f3ea9ea5fc63d5284c5bf
-m <xavier.bruhiere@gmail.com>
2013-08-31 18:34:02 +0200

    dev charm FIX, run script

--- logstash/run.sh
+++ logstash/run.sh
@@ -6,4 +6,14 @@
 # Distributed under terms of the MIT license.
 #
 
-sudo java -jar /opt/logstash/bin/logstash.jar agent -f /opt/logstash/conf/ -v
+echo -e "__ Juju logger managemet"
+echo -e "\n\t- Configuration available in /opt/logstash/conf"
+echo -e "\t- Dashboard code in ~/Kibana, file config is KibanaConfig.rb"
+echo -e "\t- Indexer used: elasticsearch in single node"
+echo -e "\n\t- Default log files monitored: $HOME/.juju/local/log/*.log\n"
+
+echo "Running logstash agent, it will take some time to initailize..."
+sudo java -jar /opt/logstash/bin/logstash.jar agent -f /opt/logstash/conf/ -v &
+sleep 10
+echo "Running dashboard..."
+sudo ruby $HOME/kibana/kibana.rb
